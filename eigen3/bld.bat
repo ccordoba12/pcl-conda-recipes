4359ff030cbbda51c85339dd6cd2da654f1a53d9
Carlos Cordoba <ccordoba12@gmail.com>
2015-08-06 11:33:32 -0500

    Eigen: Remove unneeded variable in bld.bat

--- eigen3/bld.bat
+++ eigen3/bld.bat
@@ -7,14 +7,12 @@ rem Need to handle Python 3.x case at some point (Visual Studio 2010)
 if %ARCH%==32 (
   if %PY_VER% LSS 3 (
     set CMAKE_GENERATOR="Visual Studio 9 2008"
-    set CMAKE_CONFIG="Release"
   )
 )
 
 if %ARCH%==64 (
   if %PY_VER% LSS 3 (
     set CMAKE_GENERATOR="Visual Studio 10 Win64"
-    set CMAKE_CONFIG="Release"
   )
 )
 
@@ -24,5 +22,5 @@ cmake .. -G %CMAKE_GENERATOR% ^
  -DLIB_INSTALL_DIR=%LIBRARY_LIB% ^
  -DBIN_INSTALL_DIR=%LIBRARY_BIN%
 
-cmake --build . --config %CMAKE_CONFIG% --target ALL_BUILD
-cmake --build . --config %CMAKE_CONFIG% --target INSTALL
+cmake --build . --config Release --target ALL_BUILD
+cmake --build . --config Release --target INSTALL
