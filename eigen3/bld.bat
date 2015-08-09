cbc5d0a7e3c96276495d6489995e3136420e3741
Carlos Cordoba <ccordoba12@gmail.com>
2015-08-09 13:24:51 -0500

    Eigen: Add pkg-config as a dependency
    - Also use VS 2008 Win64 as Cmake generator

--- eigen3/bld.bat
+++ eigen3/bld.bat
@@ -12,15 +12,18 @@ if %ARCH%==32 (
 
 if %ARCH%==64 (
   if %PY_VER% LSS 3 (
-    set CMAKE_GENERATOR="Visual Studio 10 Win64"
+    set CMAKE_GENERATOR="Visual Studio 9 2008 Win64"
   )
 )
+
+rem eigen3.pc (i.e. pkg-config) file is needed by python-pcl
+mkdir "%LIBRARY_LIB%\pkgconfig"
+set PKG_CONFIG_LIBDIR=%LIBRARY_LIB%
 
 cmake .. -G %CMAKE_GENERATOR% ^
  -DCMAKE_BUILD_TYPE=Release ^
  -DINCLUDE_INSTALL_DIR=%LIBRARY_INC% ^
- -DLIB_INSTALL_DIR=%LIBRARY_LIB% ^
- -DBIN_INSTALL_DIR=%LIBRARY_BIN%
+ -DEIGEN_BUILD_PKGCONFIG:BOOL=ON
 
 cmake --build . --config Release --target ALL_BUILD
 cmake --build . --config Release --target INSTALL
