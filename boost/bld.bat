414aba083e48cd4b2ccee3c53515a016f15ebdd4
Carlos Cordoba <ccordoba12@gmail.com>
2015-08-09 18:07:31 -0500

    Boost: Use version for VS 2008

--- boost/bld.bat
+++ boost/bld.bat
@@ -1,8 +1,13 @@
 set BOOSTDIR=%SRC_DIR%\boost
 
-"%SRC_DIR%\boost_1_55_0-msvc-10.0-%ARCH%.exe" /SILENT /DIR="%BOOSTDIR%"
+"%SRC_DIR%\boost_1_55_0-msvc-9.0-%ARCH%.exe" /SILENT /DIR="%BOOSTDIR%"
+if errorlevel 1 exit 1
 
 xcopy "%BOOSTDIR%\boost" "%LIBRARY_INC%\boost" /s /e /i
+if errorlevel 1 exit 1
 
-xcopy "%BOOSTDIR%"\lib%ARCH%-msvc-10.0\*vc100-mt-1_55.lib "%LIBRARY_LIB%"
-xcopy "%BOOSTDIR%"\lib%ARCH%-msvc-10.0\*vc100-mt-1_55.dll "%LIBRARY_BIN%"
+xcopy "%BOOSTDIR%"\lib%ARCH%-msvc-9.0\*vc90-mt-1_55.lib "%LIBRARY_LIB%"
+if errorlevel 1 exit 1
+
+xcopy "%BOOSTDIR%"\lib%ARCH%-msvc-9.0\*vc90-mt-1_55.dll "%LIBRARY_BIN%"
+if errorlevel 1 exit 1
