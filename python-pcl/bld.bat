abcde99d6d8796ad7d721281b831cbd155bc4bdb
Carlos Cordoba <ccordoba12@gmail.com>
2015-08-10 16:22:44 -0500

    Python-pcl: Make some renames so that it can detect the libraries it needs

--- python-pcl/bld.bat
+++ python-pcl/bld.bat
@@ -1,4 +1,13 @@
 set PKG_CONFIG_PATH=%LIBRARY_LIB%\pkgconfig
 
+rem Library renames necessary for python-pcl to compile
+for %%x in (io registration segmentation features filters sample_consensus surface search kdtree octree common) do (
+    copy %LIBRARY_LIB%\pcl_%%x_release.lib %LIBRARY_LIB%\pcl_%%x.lib
+    if errorlevel 1 exit 1
+)
+
+copy %LIBRARY_LIB%\flann_cpp_s.lib %LIBRARY_LIB%\flann_cpp.lib
+if errorlevel 1 exit 1
+
 python setup.py config --compiler=msvc build --compiler=msvc install
 if errorlevel 1 exit 1
