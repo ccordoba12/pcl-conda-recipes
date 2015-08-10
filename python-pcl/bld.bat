0ee7ed13b5f23059f66d08d2171a791af39c8ad6
Carlos Cordoba <ccordoba12@gmail.com>
2015-08-10 17:02:24 -0500

    Python-pcl: Break a long "for" line in two, in bld.bat

--- python-pcl/bld.bat
+++ python-pcl/bld.bat
@@ -2,7 +2,8 @@ rem This needed for pkg-config to detect pcl and eigen3 pc files
 set PKG_CONFIG_PATH=%LIBRARY_LIB%\pkgconfig
 
 rem Renames necessary for python-pcl to detect the libraries it needs
-for %%x in (io registration segmentation features filters sample_consensus surface search kdtree octree common) do (
+for %%x in (io registration segmentation features filters sample_consensus
+  surface search kdtree octree common) do (
     copy %LIBRARY_LIB%\pcl_%%x_release.lib %LIBRARY_LIB%\pcl_%%x.lib
     if errorlevel 1 exit 1
 )
