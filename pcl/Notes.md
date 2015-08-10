1d8c52229682f0ca9430735547c04ff8d1ed4ecf
Carlos Cordoba <ccordoba12@gmail.com>
2015-08-10 09:33:15 -0500

    PCL: Make it compile with VS 2008
    - Add several patches to make that possible

--- pcl/Notes.md
+++ pcl/Notes.md
@@ -3,3 +3,7 @@
   https://svn.boost.org/trac/boost/ticket/10272
   
   for a description of the problem.
+
+* For patches to make PCL compile with VS 2008 see
+
+  http://www.pcl-users.org/compile-errors-of-trunk-td4027294.html
--- pcl/bld.bat
+++ pcl/bld.bat
@@ -10,11 +10,9 @@ if %ARCH%==32 (
   )
 )
 
-rem Using Visual Studio 2010 for 64 bits because it's not easy to
-rem use VS 2008 Express Edition in 64 bits mode
 if %ARCH%==64 (
   if %PY_VER% LSS 3 (
-    set CMAKE_GENERATOR="Visual Studio 10 Win64"
+    set CMAKE_GENERATOR="Visual Studio 9 2008 Win64"
   )
 )
 
@@ -24,4 +22,7 @@ cmake .. -G %CMAKE_GENERATOR% ^
  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
 
 cmake --build . --config Release --target ALL_BUILD
+if errorlevel 1 exit 1
+
 cmake --build . --config Release --target INSTALL
+if errorlevel 1 exit 1
