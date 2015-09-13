aa63b7e3cc38438ec57b49128679e0f208ed41f9
Carlos Cordoba <ccordoba12@gmail.com>
2015-09-12 19:40:48 -0500

    Eigen3: Use VS 2012 to create the package on Windows

--- eigen3/bld.bat
+++ eigen3/bld.bat
@@ -6,13 +6,13 @@ cd build
 rem Need to handle Python 3.x case at some point (Visual Studio 2010)
 if %ARCH%==32 (
   if %PY_VER% LSS 3 (
-    set CMAKE_GENERATOR="Visual Studio 9 2008"
+    set CMAKE_GENERATOR="Visual Studio 11 2012"
   )
 )
 
 if %ARCH%==64 (
   if %PY_VER% LSS 3 (
-    set CMAKE_GENERATOR="Visual Studio 9 2008 Win64"
+    set CMAKE_GENERATOR="Visual Studio 11 2012 Win64"
   )
 )
 
@@ -26,4 +26,7 @@ cmake .. -G %CMAKE_GENERATOR% ^
  -DEIGEN_BUILD_PKGCONFIG:BOOL=ON
 
 cmake --build . --config Release --target ALL_BUILD
+if errorlevel 1 exit 1
+
 cmake --build . --config Release --target INSTALL
+if errorlevel 1 exit 1
